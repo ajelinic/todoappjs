751bfd3e48cf83ad91611434e53b101247f81cee
Andrej Jelinic <47660412+ajelinic@users.noreply.github.com>
2022-11-17 22:31:52 +0100

    testing

--- src/Utils/FileParsers/XmlParser.js
+++ src/Utils/FileParsers/XmlParser.js
@@ -6,6 +6,8 @@ import { UtilsFactory } from "../UtilsFactory.js";
 
 export class XmlParser {
   static parseDataFromXml(xml) {
+    console.log(xml)
+    debugger;
     let xmlHttp = UtilsFactory.createXMLHttpRequest();
     xmlHttp.open("GET", xml, false);
     xmlHttp.send();
